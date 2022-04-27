583a39e3de6d4c28e9eda58b64fe2db812c4c219
Joseph Whom <itswhom@gmail.com>
2022-04-27 15:06:43 -0700

    Add game logic

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let computerSelection = computerPlay();
-console.log(computerSelection);
+console.log(`Computer Chose: ${computerSelection}`);
+let playerSelection = "Rock";
+console.log(`Player Chose: ${playerSelection}`);
+console.log(playRPS(playerSelection, computerSelection));
 
 function computerPlay() {
     let randomPlay = Math.floor(Math.random() * 3) + 1;
@@ -11,4 +14,39 @@ function computerPlay() {
         return 'Paper';
     }
     return 'Scissors';
+}
+function playRPS(playerSelection, computerSelection) {
+    if (playerSelection == 'Rock') {
+        switch (computerSelection) {
+            case 'Rock':
+                return "Computer chose Rock... It's a tie!";
+            case 'Paper':
+                return "Paper beats Rock... You lose!";
+            case 'Scissors':
+                return "Rock beats Scissors... You win!";
+            default:
+                return "Not sure what happened here..."
+        }
+    } else if (playerSelection == 'Paper') {
+        switch (computerSelection) {
+            case 'Rock':
+                return "Paper beats Rock... You win!";
+            case 'Paper':
+                return "Computer chose Paper... It's a tie!";
+            case 'Scissors':
+                return "Scissors beats Paper... You lose!";
+            default:
+                return "Not sure what happened here...";
+        }
+    } else {
+        switch (computerSelection) {
+            case 'Rock':
+                return "Rock beats Scissors... You lose!";
+            case 'Paper':
+                return "Scissors beats Paper... You win!";
+            case 'Scissors':
+                return "Computer chose Scissors... It's a tie!"
+            default:
+        }
+    }
 }
